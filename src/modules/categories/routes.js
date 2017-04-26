cbbb8adaaefddc49253ccaa7335ba8ac88c00638
denghui <15800972458@139.com>
2017-04-26 21:56:22 +0800

    dh

--- src/modules/categories/routes.js
+++ src/modules/categories/routes.js
@@ -1,9 +1,9 @@
-import React from 'react';
+﻿import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import { Home } from 'modules/categories/Home';
 import { Edit } from 'modules/categories/Edit';
 
-export default (
+export default ( 
   <Route path="/categories" breadcrumbName="选品类目">
     <IndexRoute component={Home} />
     <Route path="/categories/edit" breadcrumbName="编辑" component={Edit} />
